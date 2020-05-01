44852e1aca2df639b2e32cb2116a6a182189c798
Josh Long <josh@joshlong.com>
2020-04-30 23:52:29 -0700

    up

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,4 @@
+cd google-sheet-ingest
 files=$HOME/Dropbox/spring-tips/spring-tips-sheet/secure/
 export PICKLED_TOKEN=$(cat $files/token.pickle | base64)
 export CREDENTIALS_JSON=$(cat $files/credentials.json)
