e8dc623a28d3f1f5c0080bb3cb3b6b5064c25450
Josh Long <josh@joshlong.com>
2020-04-30 17:35:26 -0700

    polish

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pipenv install
+echo $TICKLED_TOKEN | base64 -d > token.pickle
+echo $CREDENTIALS_JSON   > credentials.json
+ls -la
+du -hs token.pickle
+du -hs credentials.json
+pipenv run python sheets.py
