868e9423476294d5a7c4d9595c432cdeb689a40b
Josh Long <josh@joshlong.com>
2020-04-30 22:06:28 -0700

    committing spring-tips.xml at %s 1588309588

--- run.sh
+++ run.sh
@@ -3,8 +3,5 @@
 echo $PICKLED_TOKEN | base64 -d >token.pickle
 echo $CREDENTIALS_JSON >credentials.json
 pipenv install
-ls -la
-du -hs token.pickle
-du -hs credentials.json
 pipenv run python main.py
-git commit -am "committing spring-tips.xml at %s $( date +%s )"
+git commit -am "committing spring-tips.xml at %s $(date +%s)"
---
+++ run_local.sh
@@ -0,0 +1,5 @@
+files=$HOME/Dropbox/spring-tips/spring-tips-sheet/secure/
+export PICKLED_TOKEN=$(cat $files/token.pickle | base64)
+export CREDENTIALS_JSON=$(cat $files/credentials.json)
+pipenv run python main.py
+git commit -am "committing spring-tips.xml at %s $( date +%s )"
