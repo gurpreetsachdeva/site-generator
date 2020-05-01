e709ab82c4c9e0646888dc4930c0414723ef298d
Josh Long <josh@joshlong.com>
2020-04-30 22:03:41 -0700

    run local

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-base64 -v
-echo $PICKLED_TOKEN | base64 -d > token.pickle
-echo $CREDENTIALS_JSON   > credentials.json
+echo $PICKLED_TOKEN | base64 -d >token.pickle
+echo $CREDENTIALS_JSON >credentials.json
 pipenv install
 ls -la
 du -hs token.pickle
 du -hs credentials.json
 pipenv run python main.py
+git commit -am "committing spring-tips.xml at %s $( date +%s )"
